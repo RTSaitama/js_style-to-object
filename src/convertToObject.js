6bd1038748435f25a2dc13c88c1419a1deb89cb3
RTSaitama <rtsaitama@outlook.com>
2025-05-02 16:35:44 +0300

    added filter(boolen) hope will solve problem with empty strokes

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,6 +8,7 @@
 function convertToObject(sourceString) {
   return sourceString.split(';')
     .map((stroke) => stroke.trim())
+    .filter(Boolean)
     .reduce((cssObj, cssRule) => {
       const [key, ...value] = cssRule.split(':')
       cssObj[key.trim()] = value.join(':').trim()
